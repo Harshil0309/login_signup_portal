daf06c1014c0160b674a045c924bf822658c3c56
Harshil0309 <harshilgupta562@gmail.com>
2024-02-12 19:57:17 +0530

    Image upload feature added

--- my-app/src/Components/Profile/Profile.js
+++ my-app/src/Components/Profile/Profile.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import basestyle from "../Base.module.css";
+
 const Profile = ({ setUserState, username }) => {
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setImage(selectedImage);
+  };
+
   return (
     <div className="profile">
       <h1 style={{ color: "white" }}>Welcome {username} !!</h1>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageChange}
+      />
+      {image && (
+        <div>
+          <h2>Selected Image Preview:</h2>
+          <img src={URL.createObjectURL(image)} alt="Selected" />
+        </div>
+      )}
       <button
         className={basestyle.button_common}
         onClick={() => setUserState({})}
@@ -13,4 +32,5 @@ const Profile = ({ setUserState, username }) => {
     </div>
   );
 };
+
 export default Profile;
